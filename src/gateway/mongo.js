9cc56cc386b43739887c6ebc07cca35a5ee2f5d1
stolomeo <tolomeosebastian@gmail.com>
2022-05-05 18:34:47 -0400

    Rename db, define function to get collection

--- src/gateway/mongo.js
+++ src/gateway/mongo.js
@@ -1,7 +1,12 @@
 import { MongoClient } from "mongodb";
 
-const getDb = async () => {
+export const getDb = async () => {
   const client = new MongoClient(process.env.MONGO_URL);
-  await client.connect;
-  return client.db("Top 25 Stocks in the S&P 500");
+  await client.connect();
+  return client.db("Stocks");
+};
+
+export const getCollection = async () => {
+  const col = await getDb();
+  return db.collection("Top 25 Stocks in the S&P 500");
 };
