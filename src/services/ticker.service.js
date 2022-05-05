2513d7a947e4117597a91390264763e11d180221
stolomeo <tolomeosebastian@gmail.com>
2022-05-05 19:47:32 -0400

    Define functions to get multiple stocks

--- src/services/ticker.service.js
+++ src/services/ticker.service.js
@@ -5,3 +5,12 @@ export const insertStock = async (stock) => {
   const { insertedId } = await col.insertOne(stock);
   return insertedId;
 };
+
+export const getStock = async (stock) => {
+  const col = await getCollection();
+  const stock = await col.findOne({ name });
+};
+export const getStocks = async (stock) => {
+  const col = await getCollection();
+  const stock = await col.find({});
+};
