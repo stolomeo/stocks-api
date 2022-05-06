a6f8905f1722cc1f0383a062d258b31ada862c98
stolomeo <tolomeosebastian@gmail.com>
2022-05-05 22:00:41 -0400

    Add post route

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { stockRouter } from "../src/routes/ticker.route";
+import { stockRouter } from "../src/routes/ticker.route.js";
+
 dotenv.config();
+
 const app = express();
 const PORT = 3000;
 app.use(express.json());
--- src/routes/ticker.route.js
+++ src/routes/ticker.route.js
@@ -6,3 +6,11 @@ import {
   updateStock,
   deleteStock,
 } from "../services/ticker.service.js";
+
+export const stockRouter = Router();
+
+stockRouter.post("/stocks", async (req, res) => {
+  const stock = req.body;
+  const id = await insertStock(stock);
+  res.send(id.toString());
+});
