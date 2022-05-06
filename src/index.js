36795403cff7ef4a8021a1f4fe53918eafcf3e8d
stolomeo <tolomeosebastian@gmail.com>
2022-05-05 21:16:51 -0400

    Listen on port

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import { stockRouter } from "../src/routes/ticker.route";
 dotenv.config();
 const app = express();
-
+const PORT = 3000;
 app.use(express.json());
 app.use(cors());
+app.use(stockRouter);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
